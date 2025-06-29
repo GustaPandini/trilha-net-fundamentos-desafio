e76818c27496285cfd495257bd9a26e74e903ee3
GustaPandini <gugui.pandini@gmail.com>
2025-06-29 16:48:46 -0300

    Adicionado o codigo para inserir o vehiculo dentro da lista

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,9 +14,9 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+            // Adicionado o codigo para inserir o vehiculo dentro da lista
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculos.Add(Console.ReadLine());
         }
 
         public void RemoverVeiculo()
